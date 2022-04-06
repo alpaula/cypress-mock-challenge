83b00567359f06b465573f7033867e5d15ab74dc
alpaula <anna.luiza.araujo@hotmail.com>
2022-04-06 16:41:58 -0300

    adding service worker mocker

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 // Libs
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 // Components
 import Private from './private';
-import Login from './Pages/Login';
-import Layouts from './Pages/Layouts';
+import Login from './pages/Login';
+import Layouts from './pages/Layouts';
 
-ReactDOM.render(
+const { worker } = require('./mocks/browser');
+worker.start();
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" render={(props) => <Login {...props} />} />
       <Private render={(props) => <Layouts {...props} />} />
     </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/mocks/handlers.js
@@ -0,0 +1,52 @@
+// src/mocks/handlers.js
+import { rest } from 'msw'
+
+export const handlers = [
+  rest.post('/login', (req, res, ctx) => {
+    const { email, password } = req.body;
+    const isValid = email === 'cecilia@gmail.com' && password === '123456';
+
+    if (isValid) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          id: 'abc123',
+          email: 'cecilia@gmail.com',
+          firstName: 'Cecília',
+          lastName: 'Fonseca',
+          avatar: ''
+        }),
+      )
+    }
+
+    return res(
+      ctx.status(403),
+      ctx.json({
+        errorMessage: 'Not authorized',
+      }),
+    )
+  }),
+
+  rest.get('/user', (req, res, ctx) => {
+    // Check if the user is authenticated in this session
+    const isAuthenticated = sessionStorage.getItem('is-authenticated')
+
+    if (!isAuthenticated) {
+      // If not authenticated, respond with a 403 error
+      return res(
+        ctx.status(403),
+        ctx.json({
+          errorMessage: 'Not authorized',
+        }),
+      )
+    }
+
+    // If authenticated, return a mocked user details
+    return res(
+      ctx.status(200),
+      ctx.json({
+        username: 'admin',
+      }),
+    )
+  }),
+]
